d914112a2a67d537b4d95a895973653a9b2f89ce
kouhot <kouhot6210@gmail.com>
2017-10-09 10:27:45 +0900

    Renamed GoogleDriveApi to GoogleApi

--- src/google_api.rb
+++ src/google_api.rb
@@ -2,15 +2,12 @@ require 'google/api_client'
 require 'google/api_client/client_secrets'
 require 'google/api_client/auth/installed_app'
 require 'google/api_client/auth/file_storage'
-require 'net/https'
 
-class GoogleDriveApi
+class GoogleApi
   CLIENT_SECRETS_FILE = 'auth_file/client_secrets.json'.freeze
   CREDENTIAL_STORE_FILE = 'auth_file/credential_store.json'.freeze
   API_SCOPE = ['https://www.googleapis.com/auth/drive'].freeze
   APPLICATION_NAME = 'google_drive_files'.freeze
-  API_NAME = 'drive'
-  API_VERSION = 'v2'
 
   attr_reader :client, :file_storage
 
@@ -23,26 +20,6 @@ class GoogleDriveApi
     return set_auth if authorized?
     client_secrets = Google::APIClient::ClientSecrets.load(CLIENT_SECRETS_FILE)
     freshed_auth(client_secrets)
-  end
-
-  def get_files
-    drive = client.discovered_api(API_NAME, API_VERSION)
-    response = client.execute(
-      api_method: drive.files.list,
-      parameters: {
-        maxResults: 1000,
-      },
-    )
-    data = JSON.parse(response.body)
-
-    items = []
-    data['items'].each do |item|
-      row_data = {}
-      row_data[:title] = item['title']
-      items << row_data
-    end
-    puts items
-    items
   end
 
   private
